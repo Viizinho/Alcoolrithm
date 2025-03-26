abfcd92e36bf02623eb92225e885ee35bf42c0b9
André Luiz Rodrigues Castro da Nóbrega <129602730+andreluizrcn@users.noreply.github.com>
2025-03-25 21:49:57 -0300

    commit #4
    Classe Menu tem chamada na main.

---
+++ Main.java
@@ -0,0 +1,18 @@
+package src;
+
+import DAO.ClientDAO;
+import DAO.EmployeeDAO;
+import DAO.ProductDAO;
+import DAO.SaleDAO;
+import src.UI.Menu;
+import src.models.Client;
+import src.models.Employee;
+import src.models.Product;
+import src.models.Sale;
+
+public class Main {
+    public static void main(String[] args) {
+        // Remove o código de teste anterior e inicia o menu
+        Menu.main(args);
+    }
+}
